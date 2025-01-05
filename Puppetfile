093b514f41559821c32cff536d9a449ec8bdf09f
Your Name <you@example.com>
2025-01-05 16:30:06 -0500

    asdads

---
+++ Puppetfile
@@ -0,0 +1,6 @@
+forge 'http://forge.puppetlabs.com'
+moduledir 'modules'
+
+
+mod 'puppet-kmod', '4.1.0'
+mod 'thias-sysctl', '1.0.7'
--- modules/kubernetes/manifests/init.pp
+++ modules/kubernetes/manifests/init.pp
@@ -1,12 +1,41 @@
 class kubernetes (
   Boolean $controller                                     = false,
   Boolean $worker                                         = false,
+  Boolean $manage_kernel_modules                          = true,
+  Boolean $manage_sysctl_settings                         = true,
 )
  {
   if $controller {
-    package { 'vim':
-      ensure => installed,
+if $manage_kernel_modules and $manage_sysctl_settings {
+    kmod::load { 'overlay': }
+    kmod::load { 'br_netfilter':
+      before => Sysctl['net.bridge.bridge-nf-call-iptables'],
     }
+    sysctl { 'net.bridge.bridge-nf-call-iptables':
+      ensure => present,
+      value  => '1',
+      before => Sysctl['net.ipv4.ip_forward'],
+    }
+    sysctl { 'net.ipv4.ip_forward':
+      ensure => present,
+      value  => '1',
+    }
+  } elsif $manage_kernel_modules {
+    kmod::load { 'overlay': }
+    kmod::load { 'br_netfilter': }
+  } elsif $manage_sysctl_settings {
+    sysctl { 'net.bridge.bridge-nf-call-iptables':
+      ensure => present,
+      value  => '1',
+      before => Sysctl['net.ipv4.ip_forward'],
+    }
+    sysctl { 'net.ipv4.ip_forward':
+      ensure => present,
+      value  => '1',
+    }
+  }
+
+
   }
 
   if $worker {
